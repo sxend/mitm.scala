7668d28439acf9c554fea34b6fab27aff9074308
sxend <arimitsu.k+github@gmail.com>
2017-03-30 00:12:31 +0900

    add Proxy

--- src/main/scala/mitm/Proxy.scala
+++ src/main/scala/mitm/Proxy.scala
@@ -1,20 +1,46 @@
 package mitm
 
 import scala.concurrent.Future
+import scala.collection.mutable
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.server.{ HttpApp, RequestContext, RouteResult }
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl._
 
 private[mitm] trait Proxy {
-  def run(): Unit = {
+  import Proxy._
 
+  val interceptors: Set[Interceptor]
+  def run(): Unit = {
+    new HttpApp {
+      implicit private val s: ActorSystem = ActorSystem("mitm")
+      implicit private val m = ActorMaterializer()
+      implicit private val ec = s.dispatcher
+      override protected def route = { ctx: RequestContext =>
+        Http().singleRequest(ctx.request).map { res =>
+          println(s"proxy logging: $res")
+          RouteResult.Complete(res)
+        }
+      }
+    }.startServer("localhost", 8888)
   }
 }
 
 object Proxy {
+  type Interceptor = PartialFunction[(String, Future[String]), Future[String]]
   trait Builder {
-    def intercept(fn: PartialFunction[(String, Future[String]), Future[String]]): Unit = {
-      println("intercept")
+    self =>
+    private val interceptors: mutable.Set[Interceptor] = mutable.Set.empty
+    def intercept(fn: Interceptor): Unit = {
+      interceptors.update(fn, included = true)
     }
     private[mitm] def build: Proxy = {
-      new Proxy {}
+      new Proxy {
+        override val interceptors: Set[Interceptor] = self.interceptors.toSet
+      }
     }
   }
 }
